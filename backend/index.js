12a7692d24524dcfa62684f69024ef71d05f5d99
Aziz Ben Salem <azizbensalem1998@gmail.com>
2023-02-21 12:17:26 +0100

    first commit

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+
+// connexion avec la base de données
+require("dotenv").config();
+require("./config/database").connect();
+
+const app = express();
+
+// donner une exception à la partie react de consommer le backend
+var corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
+};
+
+app.use(express.json({ limit: "50mb" }));
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
