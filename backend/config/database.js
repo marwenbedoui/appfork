a33492d7e09a1c3de3d7ca04564f24925b725f67
Aziz Ben Salem <azizbensalem1998@gmail.com>
2023-02-21 12:21:16 +0100

    config db

---
+++ backend/config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const { MONGO_URI } = process.env;
+
+exports.connect = () => {
+  mongoose
+    .connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Connected to the database!");
+    })
+    .catch((err) => {
+      console.log("Database connection failed", err);
+      process.exit();
+    });
+};
--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
