d49fa834521822ebf7fe63c26c038927af63fb86
Rfifa Anouar <anouarrafifa99@gmail.com>
2023-02-23 12:35:14 +0100

    Layout and tables done

--- frontend/src/pages/testeurPage.js
+++ frontend/src/pages/testeurPage.js
@@ -1,20 +1,29 @@
 /* eslint-disable no-lone-blocks */
 import React from "react";
+import LayoutComponent from "../components/LayoutComponent";
+import TableComponent from "../components/TableComponent";
+//import TableComponent from "../components/TableComponent";
 
 export default function TesteurPage() {
   return (
-    <div>
-      <p>Testeur Page</p>
-      <button
-        onClick={() => {
-          {
-            localStorage.clear();
-            window.location.href = "/login";
-          }
-        }}
-      >
-        Deconnexion
-      </button>
-    </div>
+    // <div>
+    //   <p>Testeur Page</p>
+    //   <button
+    //     onClick={() => {
+    //       {
+    //         localStorage.clear();
+    //         window.location.href = "/login";
+    //       }
+    //     }}
+    //   >
+    //     Deconnexion
+    //   </button>
+    // </div>
+    <>
+      <LayoutComponent headerText={"Welcome Mr Tester"} mainContent={<TableComponent />}>
+
+      </LayoutComponent>
+
+    </>
   );
 }
