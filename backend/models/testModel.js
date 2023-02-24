8c71b228a06704b38e67a9df08138b149093b845
Aziz Ben Salem <azizbensalem1998@gmail.com>
2023-02-24 16:48:15 +0100

    new pull

--- backend/models/testModel.js
+++ backend/models/testModel.js
@@ -1,27 +1,41 @@
 const mongoose = require("mongoose");
 
-const testSchema = new mongoose.Schema({
-  protocol: {
-    type: String,
-    required: true,
+const testSchema = new mongoose.Schema(
+  {
+    protocol: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+    port: {
+      type: Number,
+      required: true,
+    },
+    path: {
+      type: String,
+      required: true,
+    },
+    method: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      // required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      // required: true,
+    },
   },
-  url: {
-    type: String,
-    required: true,
-  },
-  port: {
-    type: Number,
-    required: true,
-  },
-  path: {
-    type: String,
-    required: true,
-  },
-  method: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Test = mongoose.model("test", testSchema);
 
--- frontend/src/services/TesterService.jsx
+++ frontend/src/services/TesterService.jsx
@@ -1,10 +1,31 @@
 import axios from "axios";
+import jwtDecode from "jwt-decode";
 
 const API_URL = "http://localhost:5000/api/v1/tester/test";
 const token = localStorage.getItem("token");
 
 const executerTest = async (data) => {
-  const result = await axios.post(API_URL, data, {
+  const result = await axios.post(
+    API_URL,
+    {
+      protocol: data.protocol,
+      url: data.url,
+      port: data.port,
+      path: data.path,
+      method: data.method,
+      createdBy: jwtDecode(token).userId,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  return result.data;
+};
+
+const fetchAllTests = async () => {
+  const result = await axios.get(API_URL, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
@@ -14,6 +35,7 @@ const executerTest = async (data) => {
 
 const TesterService = {
   executerTest,
+  fetchAllTests,
 };
 
 export default TesterService;
