62fe03932b430375d30a529228f68763cd3c64d2
Aziz Ben Salem <azizbensalem1998@gmail.com>
2023-03-22 16:04:50 +0100

    add retest function

--- backend/routers/testerRouter.js
+++ backend/routers/testerRouter.js
@@ -7,7 +7,6 @@ router.post("/tester/test", isUser, testerController.executeTest);
 //get test
 router.get("/tester/test/", isUser, testerController.getAllTests);
 router.get("/tester/test/number", isUser, testerController.TestStatePerUser);
-router.get("/tester/test/result", isUser, testerController.getResults);
 router.get("/tester/test/all/", isUser, testerController.TestsPerUser);
 router.get("/tester/test/:id", isUser, testerController.getTestById);
 
