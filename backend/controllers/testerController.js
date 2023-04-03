df0361d12005be7b0288cfbcbcb7730c2ee8bf9a
Aziz Ben Salem <azizbensalem1998@gmail.com>
2023-04-03 13:47:04 +0100

    file management

--- backend/controllers/testerController.js
+++ backend/controllers/testerController.js
@@ -13,43 +13,7 @@ const { postTemplate } = require("../test/template/postTemplate");
 const bytes = require("bytes");
 const pidusage = require("pidusage");
 const moment = require("moment");
-
-function calculateMajority(arr) {
-  let trueCount = 0;
-  let falseCount = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === "true") {
-      trueCount++;
-    } else {
-      falseCount++;
-    }
-  }
-
-  if (trueCount > falseCount) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-function calculatePercentage(arr) {
-  let trueCount = 0;
-  let falseCount = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === "true") {
-      trueCount++;
-    } else {
-      falseCount++;
-    }
-  }
-
-  const truePercentage = (trueCount / arr.length) * 100;
-  const falsePercentage = (falseCount / arr.length) * 100;
-
-  return { truePercentage, falsePercentage };
-}
+const { calculatePercentage, calculateMajority } = require("../functions");
 
 const executeTest = async (req, res) => {
   const statsArray = [];
